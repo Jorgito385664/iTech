4ce6931d5979f146f407332ea1f8c854a24305c6
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-08-28 02:18:50 -0300

    Projeto Criado

---
+++ src/main/java/com/jorgegabriel/itech/ITechApplication.java
@@ -0,0 +1,13 @@
+package com.jorgegabriel.itech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgegabriel/itech/ITechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgegabriel.itech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
