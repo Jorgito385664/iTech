31e4133e5e770793d7d3c310a49b1b123ca0bbf0
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-08-30 03:40:32 -0300

    Endpoint /clientes/{id} disponível

--- src/main/java/com/jorgegabriel/itech/domain/Cliente.java
+++ src/main/java/com/jorgegabriel/itech/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.jorgegabriel.itech.domain.enums.TipoCliente;
 
 @Entity
@@ -32,7 +33,7 @@ public class Cliente implements Serializable{
 	private Integer tipo;
 	
 	// associações
-	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
---
+++ src/main/java/com/jorgegabriel/itech/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.jorgegabriel.itech.services;
+
+import java.util.Optional;
+
+import com.jorgegabriel.itech.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jorgegabriel.itech.domain.Cliente;
+import com.jorgegabriel.itech.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+}
