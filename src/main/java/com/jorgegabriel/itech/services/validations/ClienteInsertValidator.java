e5bab3cf5d489714a46cef376185e3215162028f
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-09-04 03:10:22 -0300

    Validação Customizada: CPF ou CNPJ na inserção de Cliente

---
+++ src/main/java/com/jorgegabriel/itech/services/validations/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.jorgegabriel.itech.services.validations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.jorgegabriel.itech.domain.enums.TipoCliente;
+import com.jorgegabriel.itech.dto.ClienteNewDTO;
+import com.jorgegabriel.itech.resources.exceptions.FieldMessage;
+import com.jorgegabriel.itech.services.validations.utils.BR;
+
+@ClienteInsert
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>(); 
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
