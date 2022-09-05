8cf7a8464b0d3ce74d35851b043ef57a15311b99
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-09-05 02:03:20 -0300

    Validação Customizada: email não repetido na atualização de Cliente

--- src/main/java/com/jorgegabriel/itech/dto/ClienteDTO.java
+++ src/main/java/com/jorgegabriel/itech/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.jorgegabriel.itech.domain.Cliente;
+import com.jorgegabriel.itech.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
